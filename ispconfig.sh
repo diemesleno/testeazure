3869eec5178419bb4a5e59fcfb03dbc7f273c656
Diemesleno Souza Carvalho <diemesleno@gmail.com>
2020-12-14 14:27:35 -0300

    Update ispconfig.sh

--- ispconfig.sh
+++ ispconfig.sh
@@ -56,12 +56,12 @@ systemctl disable spamassassin
 
 if [ "$CODENAME" != "stretch" ]; then
     apt-get -y install php7.4 php7.4-common php7.4-gd php7.4-mysql php7.4-imap php7.4-cli php7.4-cgi
-    apt-get -y install php7.4-pear php7.4-mcrypt php7.4-imagick php7.4-ldap
+    apt-get -y install php7.4-pear php7.4-mcrypt php7.4-imagick php7.4-ldap php7.4-mbstring
     apt-get -y install php7.4-curl php7.4-intl php7.4-memcache php7.4-memcached php7.4-pspell
     apt-get -y install php7.4-recode php7.4-sqlite php7.4-tidy php7.4-xmlrpc php7.4-xsl php7.4-recode
 else
     apt-get -y install php7.4 php7.4-common php7.4-gd php7.4-mysql php7.4-imap php7.4-cli php7.4-cgi
-    apt-get -y install php7.4-pear php7.4-mcrypt php7.4-imagick php7.4-mbstring php7.4-ldap
+    apt-get -y install php7.4-pear php7.4-mcrypt php7.4-imagick php7.4-mbstring php7.4-ldap php7.4-mbstring
     apt-get -y install php7.4-curl php7.4-intl php7.4-memcache php7.4-memcached php7.4-pspell php7.4-recode
     apt-get -y install php7.4-recode php7.4-sqlite3 php7.4-tidy php7.4-xmlrpc php7.4-xsl php7.4-xml
 fi
@@ -139,3 +139,4 @@ cd ispconfig3_install/install && php -q install.php
 ## Install PHPMyadmin
 ## Para Instalar o PHPMyadmin Execute o Script abaixo
 ## https://gist.github.com/jniltinho/9af397c8ddb035a322b75aecce7cdeae
+# ba46e6ac
